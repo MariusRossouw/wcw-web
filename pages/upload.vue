0884221980560e44f41a45b959bc864360600e07
Marius Rossouw <mariusrossouw@Mariuss-MacBook-Pro.local>
2018-10-02 14:09:32 +0200

    fix

--- pages/upload.vue
+++ pages/upload.vue
@@ -8,6 +8,9 @@
                 <label for="file-input">
                     <img src="@/assets/svg/upload.svg" class="logo-image"/>
                 </label>
+                <span  v-if="spinner0 == true" uk-spinner="ratio: 4.5"></span>
+                <progress v-if="bar0 == true" id="js-progressbar" class="uk-progress" :value="loaded0" :max="total0"></progress>
+                
 
                 <input id="file-input" type="file" @change="onFileChanged"/>
             </div>
@@ -19,7 +22,8 @@
                 <label for="the-file-inputB">
                     <img src="@/assets/svg/upload.svg" class="logo-image"/>
                 </label>
-
+                <span v-if="spinner1 == true" uk-spinner="ratio: 4.5"></span>
+                <progress v-if="bar1 == true" id="js-progressbar" class="uk-progress" :value="loaded1" :max="total1"></progress>
                 <input id="the-file-inputB" type="file" @change="BonFileChangedB"/>
             </div>
             <p v-if="selectedFileB" >{{selectedFileB.name}}</p>
@@ -37,7 +41,17 @@
         data() {
             return {
                 selectedFile: null,
-                selectedFileB: null
+                selectedFileB: null,
+                show0: null,
+                loaded0: null,
+                total0: null,
+                bar0: null,
+                show1: null,
+                loaded1: null,
+                total1: null,
+                bar1: null,
+                spinner0: false,
+                spinner1: false,
             }
         },
         methods: {
@@ -61,10 +75,20 @@
                 formData.append('file', this.selectedFile, this.selectedFile.name);
                 axios.post(this.$store.state.api_url + '/uploads/wine_list', formData, {
                     onUploadProgress: progressEvent => {
+                        this.bar0 = true;
+                        this.show0 = true;
+                        this.loaded0 = progressEvent.loaded;
+                        this.total0 = progressEvent.total;
+                        // this.bar0.value = this.loaded0;
+                        if(progressEvent.loaded / progressEvent.total === 1){
+                            this.spinner0 = true;
+                            this.bar0 = false;
+                        }
                     console.log(progressEvent.loaded / progressEvent.total)
                     }
                 })
                 .then(response => {
+                        this.spinner0 = false;
                         // Add entity to session in vuex
                         console.log('Response: ', response);
                         this.selectedFile = null;
@@ -82,10 +106,20 @@
                 formData.append('file', this.selectedFileB, this.selectedFileB.name);
                 axios.post(this.$store.state.api_url + '/uploads/budget', formData, {
                     onUploadProgress: progressEvent => {
+                        this.bar1 = true;
+                        this.show1 = true;
+                        this.loaded1 = progressEvent.loaded;
+                        this.total1 = progressEvent.total;
+                        // this.bar1.value = this.loaded1;
+                        if(progressEvent.loaded / progressEvent.total === 1){
+                            this.spinner1 = true;
+                            this.bar1 = false;
+                        }
                     console.log(progressEvent.loaded / progressEvent.total)
                     }
                 })
                 .then(response => {
+                        this.spinner1 = false;
                         // Add entity to session in vuex
                         console.log('Response: ', response);
                         this.selectedFileB = null;
