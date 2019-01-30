ab037358facb9214a7cfb82561f3597a09cb6224
Marius Rossouw <mariusrossouw@Mariuss-MacBook-Pro.local>
2019-01-30 13:50:26 +0200

    fix

--- store/index.js
+++ store/index.js
@@ -4,7 +4,7 @@ const createStore = () => {
   return new Vuex.Store({
     state: {
         // api_url: 'http://localhost:31001',
-        api_url: 'http://wcwines-api-stage.strathost.co.za/',
+        api_url: 'https://wcwines-api-stage.strathost.co.za/',
         session: {
             navbar: [
                 {to:"/", description:"Home"},
@@ -16,7 +16,7 @@ const createStore = () => {
         },
     },
     mutations: {
-        
+
     }
   })
 }
