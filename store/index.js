4f63bc4177af717674e891ded29cad5853b6e936
Marius Rossouw <mariusrossouw@Mariuss-MacBook-Pro.local>
2019-02-27 09:33:39 +0200

    fix

--- store/index.js
+++ store/index.js
@@ -1,24 +1,22 @@
-import Vuex from 'vuex'
+import Vuex from 'vuex';
 
 const createStore = () => {
   return new Vuex.Store({
     state: {
-        // api_url: 'http://localhost:31001',
-        api_url: 'http://wcwines-api-stage.strathost.co.za/',
-        session: {
-            navbar: [
-                {to:"/", description:"Home"},
-                // {to:"/dashboard", description:"Dashboard"},
-                // {to:"/upload", description:"Upload"}
-            ],
-            authed: false,
-            entity: {}
-        },
+      // api_url: 'http://localhost:31001',
+      api_url: 'https://wcwines-api-stage.strathost.co.za/',
+      session: {
+        navbar: [
+          { to: '/', description: 'Home' },
+          // {to:"/dashboard", description:"Dashboard"},
+          // {to:"/upload", description:"Upload"}
+        ],
+        authed: false,
+        entity: {},
+      },
     },
-    mutations: {
+    mutations: {},
+  });
+};
 
-    }
-  })
-}
-
-export default createStore
+export default createStore;
